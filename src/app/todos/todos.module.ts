768b6bd39d1d878bcc2413eb6947e797d14c08eb
iromashko <iromashko@me.com>
2021-04-13 15:43:03 +0300

    initial commit

---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { TodosComponent } from './components/todos.component';
+
+@NgModule({
+  declarations: [TodosComponent],
+  imports: [],
+})
+export class TodosModule {}
